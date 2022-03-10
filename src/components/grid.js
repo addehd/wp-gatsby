99b5236ca9ca5830ad84d33f2dfb95ac301a7cda
addehd <hej@adolfo.se>
2022-03-10 17:10:52 +0100

    npm run deploy and square images

--- src/components/grid.js
+++ src/components/grid.js
@@ -6,7 +6,32 @@ export default function grid() {
     <section className='container mx-auto pb-20 sm:pb-20 p-2 sm:p-11'>
        <h2 className="text-[red] text-5xl mt-5 mb-5 tracking-wider mx-auto text-center">NEWS – TEAM</h2>
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-1 sm:gap-4 mx-auto">
-        <div className="col-span-1 h-[11rem] sm:h-[25rem] w-100 bg-cover hue-rotate-15 bg-[url('../media/grid.webp')]">
+      <div class="square bg-[red] bg-center bg-cover hue-rotate-15 bg-[url('../media/grid.webp')]">
+        <div class="content posi">
+        </div>
+      </div>
+      <div class="square bg-[red] bg-center bg-center bg-cover hue-rotate-15 bg-[url('../media/grid.webp')]">
+        <div class="content posi">
+        </div>
+      </div>
+      <div class="square bg-[red] bg-center bg-cover hue-rotate-15 bg-[url('../media/grid.webp')]">
+        <div class="content posi">
+        </div>
+      </div>
+      <div class="square bg-[red] bg-center bg-cover hue-rotate-15 bg-[url('../media/grid.webp')]">
+        <div class="content posi">
+        </div>
+      </div>
+      <div class="square bg-[red] bg-center bg-cover hue-rotate-15 bg-[url('../media/grid.webp')]">
+        <div class="content posi">
+        </div>
+      </div>
+      <div class="square bg-[red] bg-center bg-cover hue-rotate-15 bg-[url('../media/grid.webp')]">
+        <div class="content posi">
+        </div>
+      </div>
+      
+        {/* <div className="col-span-1 h-[11rem] sm:h-[25rem] w-100 bg-cover hue-rotate-15 bg-[url('../media/grid.webp')]">
           <div className="h-full w-full gradient-bg"></div>
         </div>
         <div className="col-span-1 h-[11rem] sm:h-[25rem] w-100 bg-cover bg-[url('../media/grid.webp')]">
@@ -23,7 +48,7 @@ export default function grid() {
         </div>
         <div className="col-span-1 h-[11rem] sm:h-[25rem] w-100 bg-cover bg-[url('../media/grid.webp')]">
            <div className="h-full w-full gradient-bg"></div>
-        </div>
+        </div> */}
       </div>
     </section>
     </div>
