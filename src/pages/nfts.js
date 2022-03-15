d3d6fc54bdbf1bbea93aa6e7ce39371bd21547e5
addehd <hej@adolfo.se>
2022-03-15 22:09:46 +0100

    token symbol block and mobil friendlynese to nfts page

--- src/pages/nfts.js
+++ src/pages/nfts.js
@@ -11,14 +11,14 @@ export default function NFTS() {
   return (
     <>
       <Header />
-      <div className='bg-bjjblue'>
-        <section className='container flex flex-col-reverse sm:flex-row m-auto min-h-full sm:min-h-[85vh] justify-center'>
+      <div className='fighters bg-bjjblue'>
+        <section className='container flex flex-col-reverse sm:flex-row m-auto h-auto sm:min-h-[85vh] justify-center'>
             <div className="flex justify-center flex-col p-5 sm:p-11 w-full sm:w-2/3 w-full sm:w-1/2">
               <h1 className="text-white text-4xl sm:text-6xl 2xl:text-5xl sm:mt-11 sm:mb-11 mt-1 mb-1 font-extrabold tracking-wider">BJJ token NFTs</h1>
               <p className='text-white lg:text-xl pb-11'>You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I don't know exactly when we turned on each other, but I know that seven.</p>
             </div>
             
-          <div className="block sm:relative w-full sm:w-1/2  min-h-[33rem] top-0">
+          <div className="block sm:relative w-full sm:w-1/2  min-h-[21rem]  sm:min-h-[33rem] top-0">
             <img className="top-[4rem] sm:top-[9rem] bottom-auto sm:bottom-[9rem] right-0  left-0 m-auto h-auto absolute " src={Fighter}></img>
             <img className="pulse  top-[4rem] sm:top-[9rem] bottom-auto sm:bottom-[9rem] right-0  left-0 m-auto h-auto absolute " src={Line}></img>
             {/* <Circle /> */}
@@ -29,27 +29,28 @@ export default function NFTS() {
         </section>
       </div>
       <div>
-      <div className="bg-[#2d2d4c] bg-[url('../media/texture-lines.webp')] bg-cover">
-        <section className='container flex flex-col sm:flex-row m-auto justify-center justify-evenly pt-[7%] pb-[7%] '>
-          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4] h-auto w-[13rem]">
+      <div className="bg-[#2d2d4c] bg-[url('../media/texture-lines.webp')] bg-cover pt-[5%] pb-[5%]">
+      <h5 className='text-white text-center text-[#616dcd]'>Buchecha NFT Hologram Series</h5>
+        <section className='container grid grid-cols-2 sm:grid-cols-5 lg:grid-cols-5 gap-5 sm:gap-4 mx-auto  p-6 sm:p-11 '>
+         
+          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4]">
             <img className="w-full pt-1 pb-1" src={BuchechaNFT}/>
           </div>
-          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4] h-auto w-[13rem]">
+          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4]">
             <img className="w-full pt-1 pb-1" src={BuchechaNFT}/>
           </div>
-          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4] h-auto w-[13rem]">
+          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4]">
             <img className="w-full pt-1 pb-1" src={BuchechaNFT}/>
           </div>
-          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4] h-auto w-[13rem]">
+          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4]">
             <img className="w-full pt-1 pb-1" src={BuchechaNFT}/>
           </div>
-          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4] h-auto w-[13rem]">
+          <div className="rounded-lg bg-bjjblue border-solid border-[1px] border-[#6e84f4]">
             <img className="w-full pt-1 pb-1" src={BuchechaNFT}/>
           </div>
         </section>
         </div>
       </div>
-      {/* <Message /> */}
       <Footer />
     </>
   )
--- src/pages/token.js
+++ src/pages/token.js
@@ -4,23 +4,32 @@ import SEO from '../components/seo'
 import Footer from '../components/footer'
 import Message from "../components/message"
 import Token from "../media/bjjtoken.webp"
-
+import tokenSymbol from "../media/token-symbol.svg"
 export default function Page() {
   return (
     <>
       <Header />
       <div className='bg-bjjblue'>
         <section className='container flex flex-col-reverse sm:flex-row m-auto min-h-full sm:min-h-screen justify-center'>
-            <div className="flex justify-center flex-col p-5 sm:p-11 w-full sm:w-2/3 w-full sm:w-1/2">
+            <div className="flex justify-center flex-col p-5 sm:p-11 w-full sm:w-2/3 w-full">
               <h1 className="text-white text-4xl sm:text-6xl 2xl:text-5xl sm:mt-11 sm:mb-11 mt-1 mb-1 font-extrabold tracking-wider">BJJ token – Crypto currency backed by the BJJ community</h1>
               <p className='text-white lg:text-xl pb-11'> We are proud to present bjjtoken.io that is founded by experienced BJJ Black belts, for the needs and benefits of the BJJ community.</p>
             </div>
             <div className="mt-20 sm:mt-0 flex center w-full justify-center align-middle">
-              <img className="w-full h-auto self-center w-[84%]" src={Token}></img>
+              <img className="w-full h-auto self-center" src={Token}></img>
             </div>
         </section>
       </div>
-      <Message />
+
+      <div className='message bg-white'>
+        <div className='container flex justify-center items-center min-h-[27rem] m-auto p-7 pb-10 sm:p-11 sm-p-44 text-center'>
+          <div className='max-w-[69rem]'>
+            <h2 className='text-3xl sm:text-4xl m-5 text-[red] font-bold'><span><img className='w-[4rem] m-auto mb-2' src={tokenSymbol}/></span>BJJ token symbol</h2>
+            <p className="text-l sm:text-xl text-red-600 text-[red]"> The philosophy of Jiu-Jitsu defines the state of mind that a warrior should adopt in combat to enable victory; it is a philosophy of yielding to an opponent's force, instead of trying to oppose force with force. Ultimately, the fundamentals for good Jiu-Jitsu are also the fundamentals of good character.</p>
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </>
   )
